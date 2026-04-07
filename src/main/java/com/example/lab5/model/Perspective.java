9094fe226f7cc30d4b2e37465db9f27b08059252
Andy <e2178145@cmaisonneuve.qc.ca>
2026-04-07 00:05:52 -0400

    Can load ".ser" file now
    added the loader in menu sidebar. In addition we can now load .ser file that will show the edited version of image. If .ser file can't be loaded, shows the appropriate error

--- src/main/java/com/example/lab5/model/Perspective.java
+++ src/main/java/com/example/lab5/model/Perspective.java
@@ -6,20 +6,22 @@ import java.util.List;
 
 public class Perspective implements Observable, Serializable {
 
+    private static final long serialVersionUID = 1L;
+
     private double zoom = 1.0; // 1.0 = 100% (taille normale)
     private double transX = 0.0;
     private double transY = 0.0;
 
     private transient List<Observer> observers = new ArrayList<>();
 
-    // Méthode appelée par la TranslateCommand
+    // Methode appelee par la TranslateCommand
     public void translater(double dx, double dy) {
         this.transX += dx;
         this.transY += dy;
         notifyObservers(); // Avertit la vue de se redessiner
     }
 
-    // Méthode appelée par la ZoomCommand
+    // Methode appelee par la ZoomCommand
     public void zoomer(double facteur) {
         this.zoom *= facteur;
         notifyObservers(); // Avertit la vue de se redessiner
@@ -29,6 +31,9 @@ public class Perspective implements Observable, Serializable {
     public double getZoom() { return zoom; }
     public double getTransX() { return transX; }
     public double getTransY() { return transY; }
+    public void setZoom(double zoom) { this.zoom = zoom; }
+    public void setTransX(double transX) { this.transX = transX; }
+    public void setTransY(double transY) { this.transY = transY; }
 
     // --- Gestion du patron Observateur ---
     @Override
